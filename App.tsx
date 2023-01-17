2247b8aa9e0dd10084d2332a4e85523dea3205a6
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2023-01-17 20:00:47 -0300

    feat: add font Inter

--- App.tsx
+++ App.tsx
@@ -1,7 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+} from '@expo-google-fonts/inter';
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  });
+
+  if (!fontsLoaded) return;
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
