bd141e92991e369705947e8467756b97e7a21b91
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2023-01-23 16:30:16 -0300

    feat: add font semibold to checkbox text

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -27,7 +27,7 @@ export default function Checkbox({
         <View className="h-8 w-8 bg-zinc-900 rounded-lg" />
       )}
 
-      <Text className="text-white text-base ml-3">{title}</Text>
+      <Text className="text-white text-base font-semibold ml-3">{title}</Text>
     </TouchableOpacity>
   );
 }
