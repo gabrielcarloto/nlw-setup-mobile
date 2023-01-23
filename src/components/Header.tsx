6867ae31db7563cf441e679b88a3a016129de06d
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2023-01-23 16:06:17 -0300

    feat: add header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,14 @@
-import { View, TouchableOpacity, Text } from 'react-native';
-import Feather from '@expo/vector-icons/Feather';
+import { Text, TouchableOpacity, View } from 'react-native';
 import colors from 'tailwindcss/colors';
+
+import Feather from '@expo/vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
 import Logo from '../assets/logo.svg';
 
 export default function Header() {
+  const { navigate } = useNavigation();
+
   return (
     <View className="w-full flex-row items-center justify-between">
       <Logo />
@@ -12,6 +16,7 @@ export default function Header() {
       <TouchableOpacity
         activeOpacity={0.7}
         className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"
+        onPress={() => navigate('new')}
       >
         <Feather name="plus" color={colors.violet[500]} size={20} />
         <Text className="text-white ml-3 font-semibold text-base">Novo</Text>
