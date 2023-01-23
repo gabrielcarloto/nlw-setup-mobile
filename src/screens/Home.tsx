bd52bccfbebbdb89d614bdddc2dbe0ba798892ab
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2023-01-23 16:28:42 -0300

    feat: add habit screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,9 @@
-import { View, Text, ScrollView } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
+
+import { useNavigation } from '@react-navigation/native';
 
 import HabitDay, { DAY_SIZE } from '../components/HabitDay';
 import Header from '../components/Header';
-
 import { generateRangeDatesFromYearStart } from '../utils/generate-range-between-dates';
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
@@ -11,6 +12,8 @@ const minimumSummaryDatesSizes = 18 * 5;
 const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
 
 export default function Home() {
+  const { navigate } = useNavigation();
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
@@ -33,7 +36,10 @@ export default function Home() {
       >
         <View className="flex-row flex-wrap">
           {datesFromYearStart.map((date) => (
-            <HabitDay key={date.toString()} />
+            <HabitDay
+              onPress={() => navigate('habit', { date: date.toISOString() })}
+              key={date.toString()}
+            />
           ))}
           {amountOfDaysToFill > 0 &&
             Array.from({ length: amountOfDaysToFill }).map((_, i) => (
